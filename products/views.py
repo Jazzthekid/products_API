60aa8f69a1033bb42c289a9de35354dd233022af
Jazzthekid <schante56@gmail.com>
2023-02-23 19:39:16 -0500

    Added Functions For Delete, Put, and Post.

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from . models import Product
 from . serializers import ProductSerializer
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['GET','POST'])
@@ -17,3 +18,19 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
        
+
+@api_view(['GET','PUT','DELETE'])
+def product_detail(request,pk):
+    product = get_object_or_404(Product,pk=pk)
+    if request.method == 'GET':
+        
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
